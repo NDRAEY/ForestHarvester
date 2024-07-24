a43914e54ce7d9a4f4597a6c43c2b44c9b8942f3
NDRAEY <pikachu_andrey@vk.com>
2024-07-24 11:35:47 +0900

    What: Rektengol

--- core/src/main/java/ru/akt/forestharvester/Main.java
+++ core/src/main/java/ru/akt/forestharvester/Main.java
@@ -2,33 +2,42 @@ package ru.akt.forestharvester;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    FitViewport viewport;
+	OrthographicCamera camera;
+    ShapeRenderer shape_renderer;
+
+    public final static float WIDTH = 100;
+    public final static float HEIGHT = 16 * WIDTH / 9;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+		shape_renderer = new ShapeRenderer();
+		camera = new OrthographicCamera();
+		viewport = new FitViewport(WIDTH, HEIGHT, camera);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        shape_renderer.begin(ShapeType.Line);
+		shape_renderer.setColor(Color.RED);
+		shape_renderer.rect(10, 10, 30, 30);
+		shape_renderer.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        shape_renderer.dispose();
     }
 }
